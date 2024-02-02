9c8d2501dcd564f26a539c3adc1cfffe8378c68d
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-02 08:49:23 -0300

    chore: add simcard to endpointsSearchBar

--- app/(user)/endpoint/page.tsx
+++ app/(user)/endpoint/page.tsx
@@ -1,59 +1,63 @@
-import { fetchEndpointsFilteredByName } from "@/app/lib/actions";
-import { Breadcrumbs } from "@/app/ui/breadcrumbs";
-import EndpointsSearchBar from "@/app/ui/endpoint-search-bar";
-import { EndpointsList } from "@/app/ui/endpoints-list";
-import { EndpointsPinned } from "@/app/ui/endpoints-pinned";
+import { fetchEndpointsFilteredByName } from '@/app/lib/actions';
+import { Breadcrumbs } from '@/app/ui/breadcrumbs';
+import EndpointsSearchBar from '@/app/ui/endpoint-search-bar';
+import { EndpointsList } from '@/app/ui/endpoints-list';
+import { EndpointsPinned } from '@/app/ui/endpoints-pinned';
 
 export default async function Example({
-    searchParams,
+	searchParams,
 }: {
-    searchParams?: {
-        query?: string;
-        type?: string;
-    };
+	searchParams?: {
+		query?: string;
+		type?: string;
+	};
 }) {
-    const query = searchParams?.query || "";
-    const type = searchParams?.type || undefined;
-    const simcards = await fetchEndpointsFilteredByName(query, type);
+	const query = searchParams?.query || '';
+	const type = searchParams?.type || undefined;
+	const simcards = await fetchEndpointsFilteredByName(query, type);
 
-    return (
-        <>
-            <div className="min-h-full">
-                <div className="flex flex-col">
-                    <EndpointsSearchBar
-                        placeholder="Pesquise pelo endpoint."
-                        fieldsForSearch={[
-                            {
-                                field: "endpoint_name",
-                                label: "Nome",
-                            },
-                            {
-                                field: "sim_iccid_with_luhn",
-                                label: "Iccid",
-                            },
-                            {
-                                field: "endpoint_imei",
-                                label: "Imei",
-                            },
-                        ]}
-                    />
+	return (
+		<>
+			<div className="min-h-full">
+				<div className="flex flex-col">
+					<EndpointsSearchBar
+						placeholder="Pesquise pelo endpoint."
+						fieldsForSearch={[
+							{
+								field: 'endpoint_name',
+								label: 'Nome',
+							},
+							{
+								field: 'sim_iccid_with_luhn',
+								label: 'Iccid',
+							},
+							{
+								field: 'endpoint_imei',
+								label: 'Imei',
+							},
+							{
+								field: 'sim_msisdn',
+								label: 'Simcard',
+							},
+						]}
+					/>
 
-                    <Breadcrumbs
-                        root="/"
-                        data={[
-                            {
-                                href: "/endpoint",
-                                name: "Endpoints",
-                            },
-                        ]}
-                    />
-                    <EndpointsPinned simcards={simcards.slice(0, 4)} />
+					<Breadcrumbs
+						root="/"
+						data={[
+							{
+								href: '/endpoint',
+								name: 'Endpoints',
+							},
+						]}
+					/>
+					<EndpointsPinned simcards={simcards.slice(0, 4)} />
 
-                    <main className="flex-1">
-                        <EndpointsList simcards={simcards} />
-                    </main>
-                </div>
-            </div>
-        </>
-    );
+					<main className="flex-1">
+						<EndpointsList simcards={simcards} />
+					</main>
+				</div>
+			</div>
+		</>
+	);
 }
