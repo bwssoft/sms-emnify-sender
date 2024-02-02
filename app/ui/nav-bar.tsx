e13cd72911a44f5ae3ce3feeafc53ace6edec6f6
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-02 11:46:52 -0300

    fix: changed <button> to <Button> component

--- app/ui/nav-bar.tsx
+++ app/ui/nav-bar.tsx
@@ -4,6 +4,7 @@ import { Bars3CenterLeftIcon } from '@heroicons/react/24/outline';
 import { usePathname } from 'next/navigation';
 import { useContext, useMemo } from 'react';
 import { NavBarContext } from '../contexts/NavBarContext';
+import { Button } from './button';
 
 export function Navbar() {
 	const { setIsSideBarOpen } = useContext(NavBarContext);
@@ -24,14 +25,14 @@ export function Navbar() {
 		<>
 			{/* Search header */}
 			<div className="sticky top-0 z-10 flex h-16 flex-shrink-0 border-b border-gray-200 bg-white lg:hidden">
-				<button
+				<Button
 					type="button"
 					className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500 lg:hidden"
 					onClick={() => setIsSideBarOpen(true)}
 				>
 					<span className="sr-only">Open sidebar</span>
 					<Bars3CenterLeftIcon className="h-6 w-6" aria-hidden="true" />
-				</button>
+				</Button>
 				<div className="flex flex-1 justify-between px-4 sm:px-6 lg:px-8">
 					<div className="flex flex-1">
 						<form className="flex w-full md:ml-0" action="#" method="GET">
