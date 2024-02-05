6c69db296ba09f99c626b45f12315bc37567ad7b
Nathan985 <nathan.rodrigu3s@gmail.com>
2024-02-05 12:15:14 -0300

    feat: date styling and timezone adjustment

--- app/ui/form/MessagePageForm/Form.tsx
+++ app/ui/form/MessagePageForm/Form.tsx
@@ -1,8 +1,6 @@
-'use client'
+"use client";
 
-import {
-    sendMessagefromMessagePage,
-} from "@/app/lib/actions";
+import { sendMessagefromMessagePage } from "@/app/lib/actions";
 import FormContent from "./components/FormContent";
 import { Command } from "@/app/lib/definitions";
 import CommandHelper from "../../components/CommandHelper";
@@ -10,19 +8,25 @@ import ChatMessage from "./components/ChatMessage";
 import { Message } from "@/app/lib/emnify";
 import { Fragment, useEffect, useMemo, useState } from "react";
 import { reverseArray } from "@/app/utils/reverseArray";
+import { format, getDay, subDays, toDate } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 type DateToRenderMessageIndex = {
     [key: string]: number; // example: { '05-04-2023': 5 }
-}
+};
 
 export function Form(props: {
     endpoint_id?: string;
     commands: Array<Command>;
     messages: Array<Message>;
 }) {
-    const [datesToRenderMessageIndexes, setDatesToRenderMessageIndexes] = useState<DateToRenderMessageIndex>({});
+    const [datesToRenderMessageIndexes, setDatesToRenderMessageIndexes] =
+        useState<DateToRenderMessageIndex>({});
 
-    const reversedArray = useMemo(() => reverseArray(props.messages), [props.messages]);
+    const reversedArray = useMemo(
+        () => reverseArray(props.messages),
+        [props.messages]
+    );
 
     useEffect(() => {
         const uniqueDates = Array.from(
@@ -34,7 +38,9 @@ export function Form(props: {
         let dates: DateToRenderMessageIndex = {};
 
         for (const date of uniqueDates) {
-            dates[date] = reversedArray.findLastIndex((message) => message.submit_date.startsWith(date));
+            dates[date] = reversedArray.findLastIndex((message) =>
+                message.submit_date.startsWith(date)
+            );
         }
 
         setDatesToRenderMessageIndexes(dates);
@@ -47,23 +53,41 @@ export function Form(props: {
         url: `/message?endpoint_id=${props.endpoint_id}`,
     });
 
+    const onRenderViewDate = (date: string) => {
+        console.log(date);
+        const currentDay = getDay(new Date());
+        const yesterday = getDay(subDays(new Date(), 1));
+
+        const currentSendAt = getDay(toDate(date));
+
+        if (currentDay === currentSendAt) {
+            return "Hoje";
+        }
+
+        if (yesterday === currentSendAt) {
+            return "Ontem";
+        }
+
+        return format(toDate(date), "dd-MM-yyyy", { locale: ptBR });
+    };
+
     return (
         <div className="cols-span-4">
             <div className="relative pl-2 flex flex-col overflow-auto max-h-[80vh] scrollbar-thin scrollbar-thumb-gray-300">
                 {reversedArray.map((message, index) => (
                     <Fragment key={`${message.id}${message.payload}`}>
-                        {Object.values(datesToRenderMessageIndexes).includes(index) && (
+                        {Object.values(datesToRenderMessageIndexes).includes(
+                            index
+                        ) && (
                             <p
                                 className="w-fit p-2 my-2 flex flex-col rounded-md items-center text-gray-600 bg-gray-100 self-center justify-center text-[11px] sticky top-1"
                                 style={{ zIndex: 20 + index }}
                             >
-                                {message.submit_date.slice(0, 10).split('-').reverse().join('-')}
+                                {onRenderViewDate(message.submit_date)}
                             </p>
                         )}
 
-                        <ChatMessage
-                            content={message}
-                        />
+                        <ChatMessage content={message} />
                     </Fragment>
                 ))}
 
@@ -73,7 +97,10 @@ export function Form(props: {
             </div>
 
             <div>
-                <FormContent commands={props.commands} action={sendMessageBinded} />
+                <FormContent
+                    commands={props.commands}
+                    action={sendMessageBinded}
+                />
             </div>
         </div>
     );
--- app/ui/form/MessagePageForm/components/ChatMessage.tsx
+++ app/ui/form/MessagePageForm/components/ChatMessage.tsx
@@ -1,68 +1,67 @@
-'use client'
+"use client";
 
 import { Message } from "@/app/lib/emnify";
 import { ITypeStatus, icon } from "@/app/ui/icons";
+import { format, toDate } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 type ChatMessageProps = {
-  content: Message
-}
+    content: Message;
+};
 
 export default function ChatMessage({ content }: ChatMessageProps) {
-  return content.status.description !==
-    "DELIVERY ATTEMPT PENDING" ? (
-    <>
-      <div
-        key={content.id}
-        className="w-full flex flex-col items-end justify-end"
-      >
-        <div
-          key={content.id}
-          className="flex flex-col items-end pr-10 col-start-2 w-96"
-        >
-          <section className="flex items-center justify-center gap-1.5">
-            <div className="flex">
-              {icon(
-                content.status
-                  .description as ITypeStatus
-              )}
-            </div>
-            <p
-              className="text-[12px] text-gray-700 px-3 py-2 border bg-gray-200 rounded-lg whitespace-break-spaces overflow-wrap:break-word"
-              style={{ wordBreak: "break-all" }}
+    const onFormatHour = (date: string) => {
+        const sendDate = toDate(date);
+
+        return format(date, "HH:mm", { locale: ptBR });
+    };
+
+    return content.status.description !== "DELIVERY ATTEMPT PENDING" ? (
+        <>
+            <div
+                key={content.id}
+                className="w-full flex flex-col items-end justify-end"
             >
-              {content.payload}
-            </p>
-          </section>
-          <p className="text-gray-500 text-[9px]">
-            {content.submit_date.slice(11, 16)}
-          </p>
+                <div
+                    key={content.id}
+                    className="flex flex-col items-end pr-10 col-start-2 w-96"
+                >
+                    <section className="flex items-center justify-center gap-1.5">
+                        <div className="flex">
+                            {icon(content.status.description as ITypeStatus)}
+                        </div>
+                        <p
+                            className="text-[12px] text-gray-700 px-3 py-2 border bg-gray-200 rounded-lg whitespace-break-spaces overflow-wrap:break-word"
+                            style={{ wordBreak: "break-all" }}
+                        >
+                            {content.payload}
+                        </p>
+                    </section>
+                    <p className="text-gray-500 text-[9px]">
+                        {content.submit_date &&
+                            onFormatHour(content.submit_date)}
+                    </p>
+                </div>
+            </div>
+        </>
+    ) : (
+        <div key={content.id} className="col-span-3 flex flex-col">
+            <div className="flex flex-col items-start">
+                <section className="flex items-center justify-start gap-1.5 w-96">
+                    <p
+                        className="text-[12px] text-gray-700 px-3 py-2 border bg-gray-200 rounded-lg whitespace-break-spaces overflow-wrap:break-word"
+                        style={{ wordBreak: "break-all" }}
+                    >
+                        {content.payload}
+                    </p>
+                    <div className="flex">
+                        {icon(content.status.description as ITypeStatus)}
+                    </div>
+                </section>
+                <p className="text-gray-500 text-[9px]">
+                    {content.submit_date && onFormatHour(content.submit_date)}
+                </p>
+            </div>
         </div>
-      </div>
-    </>
-  ) : (
-    <div
-      key={content.id}
-      className="col-span-3 flex flex-col"
-    >
-      <div className="flex flex-col items-start">
-        <section className="flex items-center justify-start gap-1.5 w-96">
-          <p
-            className="text-[12px] text-gray-700 px-3 py-2 border bg-gray-200 rounded-lg whitespace-break-spaces overflow-wrap:break-word"
-            style={{ wordBreak: "break-all" }}
-          >
-            {content.payload}
-          </p>
-          <div className="flex">
-            {icon(
-              content.status
-                .description as ITypeStatus
-            )}
-          </div>
-        </section>
-        <p className="text-gray-500 text-[9px]">
-          {content.submit_date.slice(11, 16)}
-        </p>
-      </div>
-    </div>
-  );
+    );
 }
