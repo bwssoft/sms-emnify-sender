af1e222327973413310dd0bc06997aa4cc9dfab3
Nathan985 <nathan.rodrigu3s@gmail.com>
2024-02-14 08:45:49 -0300

    feat: change terminall pallete color

--- app/ui/form/CommandCreateForm/components/ModalPreview/useModalPreview.tsx
+++ app/ui/form/CommandCreateForm/components/ModalPreview/useModalPreview.tsx
@@ -1,99 +1,93 @@
-import React, { ReactNode, useState } from "react";
-import { useCommandCreateForm } from "../../useCommandCreateForm";
-import { Tooltip } from "@radix-ui/themes";
-import Badge from "@/app/ui/badge";
+import React, { ReactNode, useState } from 'react';
+import { useCommandCreateForm } from '../../useCommandCreateForm';
+import { Tooltip } from '@radix-ui/themes';
+import Badge from '@/app/ui/badge';
 import {
-    createCommandfromCoomandPage,
-    updateCommandfromCommandPage,
-} from "@/app/lib/actions";
-import { useRouter } from "next/navigation";
-import toast from "react-hot-toast";
+	createCommandfromCoomandPage,
+	updateCommandfromCommandPage,
+} from '@/app/lib/actions';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 
 const submitFunctions = {
-    updated: updateCommandfromCommandPage,
-    create: createCommandfromCoomandPage,
+	updated: updateCommandfromCommandPage,
+	create: createCommandfromCoomandPage,
 };
 
 type IKeysSubmitFunctionsType = keyof typeof submitFunctions;
 
 export const useModalPreview = () => {
-    const { state, dispatch } = useCommandCreateForm();
-    const [onPedingRequest, setOnPendingRequest] = useState<boolean>(false);
-    const router = useRouter();
-    const formState = state["CURRENT_COMMAND"];
-    const isUpadte = formState?._id !== undefined;
-    const badgeClassName = "bg-slate-800 border-slate-900 text-white";
+	const { state, dispatch } = useCommandCreateForm();
+	const [onPedingRequest, setOnPendingRequest] = useState<boolean>(false);
+	const router = useRouter();
+	const formState = state['CURRENT_COMMAND'];
+	const isUpadte = formState?._id !== undefined;
+	const badgeClassName = 'bg-gray-400 border-gray-400 text-white';
 
-    const CommnadFormater = () => {
-        const command = formState?.command;
-        const variables = formState?.variables;
-        const commandSteps = command?.split(/\[(\w+)\]/g);
+	const CommnadFormater = () => {
+		const command = formState?.command;
+		const variables = formState?.variables;
+		const commandSteps = command?.split(/\[(\w+)\]/g);
 
-        return (
-            <React.Fragment>
-                {commandSteps?.map((step, key) => {
-                    const variable = variables?.find(
-                        (props) => props.name === step
-                    );
-                    if (variable) {
-                        return (
-                            <Tooltip
-                                key={key}
-                                className="z-[99]"
-                                content={variable.description || ""}
-                            >
-                                <span>
-                                    <Badge className={badgeClassName}>
-                                        {variable.name}
-                                    </Badge>
-                                </span>
-                            </Tooltip>
-                        );
-                    }
+		return (
+			<React.Fragment>
+				{commandSteps?.map((step, key) => {
+					const variable = variables?.find((props) => props.name === step);
+					if (variable) {
+						return (
+							<Tooltip
+								key={key}
+								className="z-[99]"
+								content={variable.description || ''}
+							>
+								<span>
+									<Badge className={badgeClassName}>{variable.name}</Badge>
+								</span>
+							</Tooltip>
+						);
+					}
 
-                    return <span key={key}>{step}</span>;
-                })}
-            </React.Fragment>
-        );
-    };
+					return <span key={key}>{step}</span>;
+				})}
+			</React.Fragment>
+		);
+	};
 
-    const onHandleSubmit = async () => {
-        try {
-            setOnPendingRequest(true);
+	const onHandleSubmit = async () => {
+		try {
+			setOnPendingRequest(true);
 
-            if (!formState) {
-                return;
-            }
+			if (!formState) {
+				return;
+			}
 
-            const typeAction: IKeysSubmitFunctionsType = isUpadte
-                ? "updated"
-                : "create";
+			const typeAction: IKeysSubmitFunctionsType = isUpadte
+				? 'updated'
+				: 'create';
 
-            await submitFunctions[typeAction](formState);
+			await submitFunctions[typeAction](formState);
 
-            router.push("/command", { scroll: false });
-            toast.success(
-                `Comando ${isUpadte ? "atualizado" : "criado"} com sucesso!`
-            );
-        } catch (error) {
-            toast.error(
-                `Ocorreu um erro ao ${
-                    isUpadte ? "atualizar" : "criar"
-                } o comando`
-            );
-        } finally {
-            setOnPendingRequest(false);
-            dispatch({ type: "MODAL_PREVIEW_COMMAND", payload: false });
-        }
-    };
+			router.push('/command', { scroll: false });
+			toast.success(
+				`Comando ${isUpadte ? 'atualizado' : 'criado'} com sucesso!`,
+			);
+		} catch (error) {
+			toast.error(
+				`Ocorreu um erro ao ${isUpadte ? 'atualizar' : 'criar'} o comando`,
+			);
+		} finally {
+			setOnPendingRequest(false);
+			dispatch({ type: 'MODAL_PREVIEW_COMMAND', payload: false });
+		}
+	};
 
-    return {
-        state,
-        dispatch,
-        formState,
-        CommnadFormater,
-        badgeClassName,
-        onHandleSubmit,
-        onPedingRequest,
-    };
+	return {
+		state,
+		dispatch,
+		formState,
+		CommnadFormater,
+		badgeClassName,
+		onHandleSubmit,
+		onPedingRequest,
+	};
 };
