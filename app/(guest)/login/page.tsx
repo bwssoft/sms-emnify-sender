f60cc0282374da27e737bce2cfae10e340f8c74b
bwssoft <development@bwsiot.com>
2025-03-26 14:45:17 -0300

    feat: show error

--- app/(guest)/login/page.tsx
+++ app/(guest)/login/page.tsx
@@ -1,11 +1,12 @@
-"use client"
+'use client';
 
-import { userAuthenticate } from "@/app/lib/actions";
-import { Button } from "@bwsoft/button";
-import { Input } from "@bwsoft/input"
+import { userAuthenticate } from '@/app/lib/actions';
+import { Button } from '@bwsoft/button';
+import { Input } from '@bwsoft/input';
 
-import Logo from "../../../public/logotipo.png";
-import Image from "next/image";
+import Logo from '../../../public/logotipo.png';
+import Image from 'next/image';
+import { useFormState } from 'react-dom';
 
 /*
   This example requires some changes to your config:
@@ -22,70 +23,77 @@ import Image from "next/image";
   ```
 */
 export default function Example() {
-  return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm flex flex-col items-center">
-          <Image src={Logo} alt="Logotipo" width={120} />
+	const [errorMessage, formAction] = useFormState(userAuthenticate, undefined);
+	return (
+		<>
+			<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+				<div className="sm:mx-auto sm:w-full sm:max-w-sm flex flex-col items-center">
+					<Image src={Logo} alt="Logotipo" width={120} />
 
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Faça login na sua conta
-          </h2>
-        </div>
+					<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+						Faça login na sua conta
+					</h2>
+				</div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action={userAuthenticate} method="POST">
-            <div>
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Nome do usuário
-              </label>
-              <div className="mt-2">
-                <Input.Field
-                  id="username"
-                  name="username"
-                  type="username"
-                  autoComplete="username"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Senha
-              </label>
-              <div className="mt-2">
-                <Input.Field
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="flex justify-end">
-              <a href="/passwordRecovery">Esqueceu a senha?</a>
-            </div>
+				<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+					<form className="space-y-6" action={formAction} method="POST">
+						<div>
+							<label
+								htmlFor="username"
+								className="block text-sm font-medium leading-6 text-gray-900"
+							>
+								Nome do usuário
+							</label>
+							<div className="mt-2">
+								<Input.Field
+									id="username"
+									name="username"
+									type="username"
+									autoComplete="username"
+									required
+									className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+						<div>
+							<label
+								htmlFor="password"
+								className="block text-sm font-medium leading-6 text-gray-900"
+							>
+								Senha
+							</label>
+							<div className="mt-2">
+								<Input.Field
+									id="password"
+									name="password"
+									type="password"
+									autoComplete="password"
+									required
+									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+						<div className="flex justify-end">
+							<a href="/passwordRecovery">Esqueceu a senha?</a>
+						</div>
 
-            <div>
-              <Button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Login
-              </Button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+						{errorMessage ? (
+							<p className="text-sm text-red-500">{errorMessage}</p>
+						) : (
+							<></>
+						)}
+
+						<div>
+							<Button
+								type="submit"
+								className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							>
+								Login
+							</Button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</>
+	);
 }
--- app/lib/actions/authenticate.ts
+++ app/lib/actions/authenticate.ts
@@ -1,59 +1,76 @@
-"use server";
+'use server';
 
 import * as repository from '../repository';
 import * as emnify from '../emnify';
-import { auth, signIn, signOut } from "@/auth"
+import { auth, signIn, signOut } from '@/auth';
 
-import bcrypt from "bcrypt"
+import bcrypt from 'bcrypt';
+import { AuthError } from 'next-auth';
 
-export async function userAuthenticate(formData: FormData) {
-  const { username, password } = Object.fromEntries(formData.entries())
-  const result = await emnify.authenticate({ token: process.env.BWS_EMNIFY_AUTH_TOKEN })
-  if (!result) return
-  await signIn("credentials", {
-    token: result.token,
-    username,
-    password
-  })
+export async function userAuthenticate(
+	prevState: string | undefined,
+	formData: FormData,
+) {
+	const { username, password } = Object.fromEntries(formData.entries());
+	const result = await emnify.authenticate({
+		token: process.env.BWS_EMNIFY_AUTH_TOKEN,
+	});
+	if (!result) return 'Emnify Token Error';
+	try {
+		await signIn('credentials', {
+			token: result.token,
+			username,
+			password,
+		});
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case 'CredentialsSignin':
+					return 'Senha inválida.';
+				default:
+					return 'Algo deu errado.';
+			}
+		}
+		throw error;
+	}
 }
 
 export async function isAdmOrSupport(): Promise<boolean> {
-  const data = await auth()
-  if(!data){
-      return false
-  }
-  return data.user.role ===  'adm' || data.user.role === "support"
+	const data = await auth();
+	if (!data) {
+		return false;
+	}
+	return data.user.role === 'adm' || data.user.role === 'support';
 }
 
 export async function userLogout() {
-  try {
-    await signOut({ redirectTo: "/login" });
-  } catch (error) {
-    throw error;
-  }
+	try {
+		await signOut({ redirectTo: '/login' });
+	} catch (error) {
+		throw error;
+	}
 }
 
 export async function changeUserPassword(formData: FormData) {
-  const { new_password } = Object.fromEntries(formData.entries())
-  const hashPassword = await bcrypt.hash(new_password as string, 10)
-  return await repository.updateUser({
-    password: hashPassword
-  })
+	const { new_password } = Object.fromEntries(formData.entries());
+	const hashPassword = await bcrypt.hash(new_password as string, 10);
+	return await repository.updateUser({
+		password: hashPassword,
+	});
 }
 
 export async function recoverUserPassword(formData: FormData) {
-  const entries = Object.fromEntries(formData.entries());
-  const username = entries.username.toString();
-  const new_password = entries.new_password.toString();
+	const entries = Object.fromEntries(formData.entries());
+	const username = entries.username.toString();
+	const new_password = entries.new_password.toString();
 
-
-  const user = await repository.listUserByUsername(username);
-  if (!user) {
-    throw new Error('Usuário não encontrado');
-  }
-  const hashPassword = await bcrypt.hash(new_password, 10)
-  return await repository.updateUser({
-    username: username,
-    password: hashPassword
-  })
+	const user = await repository.listUserByUsername(username);
+	if (!user) {
+		throw new Error('Usuário não encontrado');
+	}
+	const hashPassword = await bcrypt.hash(new_password, 10);
+	return await repository.updateUser({
+		username: username,
+		password: hashPassword,
+	});
 }
